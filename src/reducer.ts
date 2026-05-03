1c017b1d6428b1e5e0f36e5a5dac433a68250147
arsia <ardalan.wrx@gmail.com>
2026-05-03 18:36:15 -0400

    feat: add state machine: types, interface, reducer, actions

---
+++ src/reducer.ts
@@ -0,0 +1,22 @@
+import type { Note, Action } from "./types";
+
+export function notesReducer(state: Note[], action: Action): Note[] {
+  switch (action.type) {
+    case "CREATE":
+      return [...state, action.note];
+    case "MOVE":
+      return state.map((note) =>
+        note.id === action.id ? { ...note, x: action.x, y: action.y } : note,
+      );
+    case "EDIT":
+      return state.map((note) =>
+        note.id === action.id ? { ...note, text: action.text } : note,
+      );
+    case "RESIZE":
+      return state.map((note) =>
+        note.id === action.id ? { ...note, w: action.w, h: action.h } : note,
+      );
+    case "DELETE":
+      return state.filter((note) => note.id !== action.id);
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export interface Note {
+  id: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+  text: string;
+  color: number; //will have a dict
+}
+
+export type Action =
+  | { type: "CREATE"; note: Note }
+  | { type: "MOVE"; id: string; x: number; y: number }
+  | { type: "RESIZE"; id: string; w: number; h: number }
+  | { type: "EDIT"; id: string; text: string }
+  | { type: "DELETE"; id: string };

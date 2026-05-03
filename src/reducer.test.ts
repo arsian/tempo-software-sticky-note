17fb4a95829315d576fa2c9fbff2472048e357a0
arsia <ardalan.wrx@gmail.com>
2026-05-03 18:55:29 -0400

    test: test MOVE

--- src/reducer.test.ts
+++ src/reducer.test.ts
@@ -27,8 +27,26 @@ describe("notesReducer", () => {
     expect(result).toEqual([baseNote, secondNote]);
   });
 
-  //   test("MOVE updates only the target note", () => {});
-  //   test("MOVE on a non-existant note/id is no-op", () => {});
+  test("MOVE updates only the target note", () => {
+    const secondNote: Note = { ...baseNote, id: "2", x: 50, y: 50 };
+    const result = notesReducer([baseNote, secondNote], {
+      type: "MOVE",
+      id: "2",
+      x: 200,
+      y: 300,
+    });
+    //make sure only note id:2 is moved
+    expect(result).toEqual([baseNote, { ...secondNote, x: 200, y: 300 }]);
+  });
+  test("MOVE on a non-existant note/id is no-op", () => {
+    const result = notesReducer([baseNote], {
+      type: "MOVE",
+      id: "999",
+      x: 0,
+      y: 0,
+    });
+    expect(result).toEqual([baseNote]);
+  });
 
   //   test("RESIZE updates only the target note", () => {});
 
